09213396042ae5f23f0c34fc41ba16cbb12ecb1c
Nghia Nguyen <nguyenn2001@gmail.com>
2026-04-18 22:17:01 -0400

    update url type

--- src/components/Map/utils/dataParsing.ts
+++ src/components/Map/utils/dataParsing.ts
@@ -1,4 +1,4 @@
-export function toSpotifyEmbed(url): string {
+export function toSpotifyEmbed(url: string): string {
   const match = url.match(/open\.spotify\.com\/(playlist|track|album|artist|episode|show)\/([a-zA-Z0-9]+)/);
   if (!match) return "";
   const [, type, id] = match;
